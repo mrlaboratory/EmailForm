b2a45fb8a39640b83ca501d448caf1bb6bdc326c
mrlaboratory <mrlaboratory23@gmail.com>
2021-06-11 12:48:14 +0600

    added all html code for email

---
+++ script.js
@@ -0,0 +1,3 @@
+var posttitleLink ="mailto:"+adminemail+"?Subject="+postTitlev+"&body=Hello Dear !! Read Our Latest Post : "+postTitlev+"        Link : "+postlinkv;
+// var  link = posttitleLink.toString();
+window.location.href = posttitleLink;
